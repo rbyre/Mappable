731bd039b2d9902afce3d8e194842bbef6c42a7c
Runar Byre <runar.byre@gmail.com>
2021-12-09 20:20:58 +0100

    first commit

---
+++ src/CustomMap.ts
@@ -0,0 +1,33 @@
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: mappable
+        ? {
+            lat: mappable.location.lat,
+            lng: mappable.location.lng,
+          }
+        : null,
+    });
+  }
+}
